84e289e122adfe1bc503f665b918e3560c5b2b97
ashish1sasmal <ashishsasmal1@gmail.com>
2020-01-28 17:33:38 +0530

    background-process-implmented

--- news/views.py
+++ news/views.py
@@ -1,15 +1,44 @@
 from django.shortcuts import render
 import schedule
-# Create your views here.
+import os
 import time
+import smtplib
+from bs4 import BeautifulSoup
+import requests
+
+EMAIL_ID = os.environ.get('EMAIL_ID')
+EMAIL_PASS = os.environ.get('EMAIL_PASS')
+
+def send_email(subject,msg,email_id):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(EMAIL_ID,EMAIL_PASS)
+        print("here")
+        message = f'Subject:{subject}\n\n{msg}'
+        server.sendmail(EMAIL_ID,email_id,message)
+        server.quit()
+        print("success!")
+    except:
+        print("email failed!")
 
 def job():
-    print("I'm working...")
+    subject = "HELlo"
+    msg="come tommorrow"
+    send_email(subject,msg,"ashishsasmal1@gmail.com")
 
-schedule.every(5).seconds.do(job)
+schedule.every(10).seconds.do(job)
 
 def home(request):
     while True:
         schedule.run_pending()
         time.sleep(1)
     return render(request,'news/home.html')
+
+def test(request):
+    
+    return render(request,'news/test.html')
+
+
+############$$$$$$$$$$$$$$ W E B - S C R A P I N G $$$$$$$$$$$$$$#########################
