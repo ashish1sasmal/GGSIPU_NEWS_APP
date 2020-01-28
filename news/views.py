e9a1ca3d92a2c76ec27942bcd122560b38a12783
ashish1sasmal <ashishsasmal1@gmail.com>
2020-01-28 20:13:48 +0530

    scrapped100%

--- news/views.py
+++ news/views.py
@@ -10,6 +10,7 @@ from datetime import date
 
 EMAIL_ID = os.environ.get('EMAIL_ID')
 EMAIL_PASS = os.environ.get('EMAIL_PASS')
+day=date.today().strftime("%d-%m-%Y")
 
 def send_email(subject,msg,email_id):
     try:
@@ -39,22 +40,42 @@ def test(request):
     return render(request,'news/home.html')
 
 def home(request):
+    latest=scrap_notices()
+    print(latest)
 
+    if latest!=[]:
+        form = LastNotice(title=latest[0][0],url=latest[0][1])
+        form.save()
+        print('success')
+        send_email("ggsipu notice",str(latest),"ashishsasmal1@gmail.com")
+    else:
+        print('no new notice')
     return render(request,'news/test.html')
 
 
 ############$$$$$$$$$$$$$$ W E B - S C R A P I N G $$$$$$$$$$$$$$#########################
 
-source  = requests.get('http://www.ipu.ac.in/notices.php').text
-soup = BeautifulSoup(source,'lxml')
-notices = soup.find_all('tr')
 
-day=date.today().strftime("%d-%m-%Y")
 
-for notice in notices:
-    l=notice('td')
-    if len(l)>1:
-        if (l[-1].get_text()==day):
-            print(l[0].a.get_text())
-            print("http://www.ipu.ac.in"+(l[0].a)['href'])
-            print()
+def scrap_notices():
+    print('here')
+    source  = requests.get('http://www.ipu.ac.in/notices.php').text
+    soup = BeautifulSoup(source,'lxml')
+    notices = soup.find_all('tr')
+
+    latest=[]
+    index=0
+    for notice in notices:
+        print('y')
+        l=notice('td')
+        if len(l)>1:
+            text=l[0].a.get_text()
+            print(LastNotice.objects.last().title)
+            if (l[-1].get_text()==day) and LastNotice.objects.last().title!=text:
+                latest.append([text,("http://www.ipu.ac.in"+(l[0].a)['href'])])
+                index+=1
+
+            else:
+                break
+
+    return latest
